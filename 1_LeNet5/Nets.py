0fd51cd3ae70ca3e52b86805725630748b55a5bc
Yin Yingda <yingda.yin@gmail.com>
2018-07-01 22:44:42 +0800

    Networks build
    Just containing LeNet5 right now

---
+++ 1_LeNet5/Nets.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+
+
+class LeNet5(nn.Module):
+    """
+    Input - 1x32x32
+    C1 - 6@28x28 (5x5 kernel)
+    tanh
+    S2 - 6@14x14 (2x2 kernel, stride 2)
+    C3 - 16@10x10 (5x5 kernel, complicated shit)
+    tanh
+    S4 - 16@5x5 (2x2 kernel, stride 2)
+    C5 - 120@1x1 (5x5 kernel)
+    F6 - 84
+    tanh
+    F7 - 10 (Output)
+    """
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(6, 16, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(16, 120, kernel_size=(5, 5)),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10),
+            nn.LogSoftmax(1)       # 0-dim是batchsize，1-dim是真正的数据，在1-dim进行logsoftmax操作
+        )
+
+    def forward(self, img):
+        output = self.conv(img)
+        output = output.view(output.size(0),-1)
+        output = self.fc(output)
+        return output
